7c4eb1cc186b809bc0095bc56d08c9d236cfc384
Ramanajee Kandregula <ramanajee516@live.in>
2015-11-22 19:22:57 +0530

    ChangeEventDemo

---
+++ ChangeEventDemo/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChangeEventDemo.Models
+{
+    public class Country
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<States> States { get; set; }
+    }
+}
---
+++ ChangeEventDemo/Models/States.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChangeEventDemo.Models
+{
+    public class States
+    {
+        public string StateId { get; set; }
+        public string Name { get; set; }
+        public string CountryId { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public virtual Country Country { get; set; }
+    }
+}
